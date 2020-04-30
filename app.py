a7fea4f37d7159cc2f32bbf6910746aca4271465
The Floating String <laurencel2001@gmail.com>
2020-04-30 00:37:02 -0400

    Fix bug for job categories

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from cryptography.fernet import Fernet
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, func
 from sqlalchemy.orm import sessionmaker
 
 
@@ -67,7 +67,7 @@ def request_loader(request):
 	password = request.form["password"]
 
 	if bcrypt.checkpw(password.encode(), \
-			session.query(Employer).filter_by(email=email).first().password_hash.encode()):
+			session.query(Employer).filter(func.lower(Employer.email)==func.lower(email)).first().password_hash.encode()):
 		user.set_is_authenticated(True)
 
 	else:
@@ -102,8 +102,9 @@ def view_jobs():
 			for tag in job.tags:
 				job_name += f' <span class="badge badge-warning">{tag}</span>'
 		temp_list = [job_name, job.company_name, job.description, job_button]
-		if "paid-employment" in job.categories: 
-			all_jobs.append(temp_list)
+		if job.categories is not None:
+			if "paid-employment" in job.categories: 
+				all_jobs.append(temp_list)
 		else:
 			all_volunteering.append(temp_list)
 
@@ -132,7 +133,7 @@ def employer_login():
 		list_of_results = []
 
 		if bcrypt.checkpw(password.encode(), \
-			session.query(Employer).filter_by(email=email).first().password_hash.encode()):
+			session.query(Employer).filter(func.lower(Employer.email)==func.lower(email)).first().password_hash.encode()):
 
 			user = User()
 			user.id = email
